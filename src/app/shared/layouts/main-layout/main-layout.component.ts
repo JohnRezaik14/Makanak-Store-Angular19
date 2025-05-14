3c2a7258a8c78df1aba2c58e8c91ba8081de89ec
John Rezaik <johnrezaik@gmail.com>
2025-05-14 20:07:46 +0300

    product-card styling

--- src/app/shared/layouts/main-layout/main-layout.component.ts
+++ src/app/shared/layouts/main-layout/main-layout.component.ts
@@ -1,20 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
-
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+export type Product = {
+  id: number;
+  title: string;
+  image: string;
+  brand: string;
+  category: string;
+  color: string;
+  discription: string;
+  discount: string;
+  model: string;
+  price: number;
+};
 @Component({
   selector: 'app-main-layout',
   templateUrl: './main-layout.component.html',
   styleUrls: ['./main-layout.component.css'],
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, CommonModule, ButtonModule],
 })
-export class MainLayoutComponent implements OnInit {
-  products: [] = [];
+export class MainLayoutComponent implements OnInit, OnChanges {
+  products: Product[] = [];
   constructor() {}
-
-  ngOnInit() {
-    fetch('https://fakestoreapi.in/api/products')
-      .then((res) => res.json())
-      .then((res) => (this.products = res.products));
+  fetchData = async () => {
+    const response = await fetch('https://fakestoreapi.in/api/products');
+    const data = await response.json();
+    return data.products;
+  };
+  async ngOnInit() {
+    const result = await this.fetchData();
+    this.products = result;
+    console.log(result);
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(this.products);
   }
 }
