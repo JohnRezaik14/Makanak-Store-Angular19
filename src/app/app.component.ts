351902b14ef179bd59159af1df6a82eb667e8825
John Rezaik <johnrezaik@gmail.com>
2025-05-16 20:52:55 +0300

    Products layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './features/home/pages/home/home.component';
 import { AboutComponent } from './features/about/pages/about/about.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HttpClientModule, RouterOutlet, HomeComponent, AboutComponent],
+  imports: [HttpClientModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
